f7c59d5de93cb20e41389bd4b8916a20da672794
Evgeniya <kossevgenia@gmail.com>
2023-02-03 00:16:07 +0300

    Решение задачи 41

--- Tusk41/Program.cs
+++ Tusk41/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3???
+
+Console.Write("Введите целые числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int result = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) 
+    {
+        result++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во чисел больше 0: {result}");
