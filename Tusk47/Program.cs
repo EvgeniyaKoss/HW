06961799315c9c9b00e5a72302a8ae581b30485a
Evgeniya <kossevgenia@gmail.com>
2023-02-03 16:26:30 +0300

    Решение задачи 47

--- Tusk47/Program.cs
+++ Tusk47/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+
+(int, int) GetNumberByUser()
+{
+  Console.WriteLine("Введите количество строк массива: ");
+  int rows = int.Parse(Console.ReadLine());
+  Console.WriteLine("Введите количество столбцов  массива: ");
+  int columns = int.Parse(Console.ReadLine());
+  return (rows, columns);
+}
+
+double[,] CreateMatrix(int rows, int columns)
+{
+  double[,] matr = new double[rows, columns];
+  return matr;
+}
+
+void FillMatrix(double[,] matr)
+
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                matr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+                Console.Write($"| {matr[i, j]} |");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+(int rows, int columns) size = GetNumberByUser();
+double[,] matrix = CreateMatrix(size.rows, size.columns);
+FillMatrix(matrix);
