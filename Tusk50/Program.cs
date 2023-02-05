bff6dc59320811d70bfac420294115d47bea9ca5
Evgeniya <kossevgenia@gmail.com>
2023-02-06 02:43:24 +0300

    Решение задач 47,50,52

--- Tusk50/Program.cs
+++ Tusk50/Program.cs
@@ -1,10 +1,34 @@
 ﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 //и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-//Например, задан массив:
 
-//1 4 7 2
-//5 9 2 3 
-//8 4 2 4
-
-//17 -> такого числа в массиве нет
+int[,] CreateFillArray()
+{
+    Console.WriteLine("Введите количество строк и столбцов в таблице (ввод числа через enter): ");
+    int[,] arr = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+    Console.WriteLine();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Random.Shared.Next(100);
+            Console.Write($"|{arr[i, j], 3} | ");
+        }
+        Console.WriteLine();
+    }
+    return arr;
+}
+void GetElementArray(int[,] array)
+{
+    Console.Write("Введите индекс строки: ");
+    int line = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите индекс столбца: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    if (line < array.GetLength(0) & colums < array.GetLength(1))
+    {
+        Console.WriteLine($"Элемент массива по индексу ({line},{colums}) = {array[line,colums]}");
+    }
+    else Console.WriteLine($"Элемента ({line},{colums}) не существует");
+}
+int[,] arr = CreateFillArray();
+GetElementArray(arr);
--- Tusk52/Program.cs
+++ Tusk52/Program.cs
@@ -8,3 +8,48 @@
 //8 4 2 4
 
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+(int, int) GetNumberByUser()
+{
+    Console.WriteLine("Введите количество строк массива(m): ");
+    int rows = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов  массива (n): ");
+    int columns = int.Parse(Console.ReadLine());
+    return (rows, columns);
+}
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i, j] = Convert.ToInt32(new Random().Next(10));
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return arr;
+}
+void Average(int[,] array)
+{
+    double midlNumbers = 0;
+    Console.WriteLine();
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            midlNumbers += array[i,j];
+        }
+        Console.WriteLine ($"{Math.Round(midlNumbers / array.GetLength(1), 2)}");
+        midlNumbers = 0;
+    }
+}
+
+(int rows, int columns) size = GetNumberByUser();
+int[,] matrix = FillMatrix(size.rows, size.columns);
+Console.Write("Cреднее арифметическое каждого столбца: ");
+Average(matrix);
+
