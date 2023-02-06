777e210961408b875946ff90824e612a59215b8f
Evgeniya <kossevgenia@gmail.com>
2023-02-07 02:41:02 +0300

    Решение 56 задачи

--- Tusk56/Program.cs
+++ Tusk56/Program.cs
@@ -17,8 +17,48 @@
   return (rows, columns);
 }
 
-double[,] CreateMatrix(int rows, int columns)
+int[,] FillMatrix(int row, int column)
 {
-  double[,] matr = new double[rows, columns];
-  return matr;
+  int[,] arr = new int[row, column];
+
+  for (int r = 0; r < row; r++)
+  {
+    for (int c = 0; c < column; c++)
+    {
+      arr[r, c] = Convert.ToInt32(new Random().Next(0, 10));
+      Console.Write(arr[r, c] + " ");
+    }
+    Console.WriteLine();
+  }
+  return arr;
 }
+string MinSumLine(int[,] matr)
+{
+  int tempr = 0;
+  int mintemp = 0;
+  for (int c = 0; c < matr.GetLength(1); c++)
+  {
+    mintemp += matr[0, c];
+  }
+
+  for (int r = 1; r < matr.GetLength(0); r++)
+  {
+    int sum = 0;
+    for (int c = 0; c < matr.GetLength(1); c++)
+    {
+      sum += matr[r, c];
+    }
+    if (mintemp > sum)
+    {
+      mintemp = sum;
+      tempr = r;
+    }
+  }
+  Console.WriteLine($"Минимальная сумма в строке: {mintemp}");
+  return $"{tempr}";
+}
+
+(int row, int column) size = GetNumberByUser();
+int[,] matrix = FillMatrix(size.row, size.column);
+string minsum = MinSumLine(matrix);
+Console.WriteLine($"Номер строки с наименьшей суммой элементов: {minsum} ");
