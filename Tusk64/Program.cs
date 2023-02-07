29cc3e30c60a96d8935835e877d69ab71a7cb4e9
Evgeniya <kossevgenia@gmail.com>
2023-02-07 03:11:47 +0300

    Решение 64,66 задачи

--- Tusk64/Program.cs
+++ Tusk64/Program.cs
@@ -4,3 +4,22 @@
 
 //N = 5 -> "5, 4, 3, 2, 1"
 //N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int GetNumberByUser()
+{
+  Console.Write("Введите значение n: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+  return num;
+}
+
+void PrintNumbersFrom1ToN(int n)
+{
+  if (n != 1) Console.Write($"{n}, ");
+  else Console.Write($"{n}");
+  if (n == 1) return;
+  n--;
+  PrintNumbersFrom1ToN(n);
+}
+
+int num = GetNumberByUser();
+PrintNumbersFrom1ToN(num);
